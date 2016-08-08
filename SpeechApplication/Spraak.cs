e35b05f066cc7ffb834bb346bf74e3d0e107a95b
whoami1995 <benjamindendas@msn.com>
2016-08-08 14:28:36 +0200

    Rewrite 1.0
    Rewriting the whole application to seperated classes (Currently unknown
    if possible to pass EventHandler<SpeechRecognizedEventArgs> with a
    method) If yes, rewrite later to 1 class instead of 2.

---
+++ SpeechApplication/Spraak.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpeechApplication
+{
+    public class Spraak
+    {
+
+        private SpeechSynthesizer Synth = new SpeechSynthesizer();
+        private PromptBuilder builder = new PromptBuilder();
+        private SpeechRecognitionEngine engine = new SpeechRecognitionEngine();
+        private Choices choices = new Choices();
+        private GrammarBuilder gramBuilder = null;
+        private Grammar gram = null;
+        private Spraak2 program = null;
+        private bool booted = false;
+
+        public void Func()
+        {
+            program = new Spraak2();
+            Synth.SelectVoiceByHints(VoiceGender.Male);
+            choices.Add(new string[] { "Boot", "Shut" });
+            gramBuilder = new GrammarBuilder(choices);
+            gramBuilder.Culture = Thread.CurrentThread.CurrentCulture;
+            gram = new Grammar(gramBuilder);
+
+            try
+            {
+                engine.RequestRecognizerUpdate();
+                engine.LoadGrammar(gram);
+                engine.SpeechRecognized += engine_SpeechRecognized;
+                engine.SetInputToDefaultAudioDevice();
+                engine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+        void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch(e.Result.Text.ToString())
+            {
+                case "Boot":
+                    if(!booted)
+                    {
+                        ReturnAnswer(builder, Synth, "Activating Recognizer 2");
+                        program.Start();
+                        booted = true;
+                    }      
+                    break;
+                case "Shut":
+                    if(booted)
+                    {
+                        ReturnAnswer(builder, Synth, "Deactivating Recognizer 2");
+                        program.Stop();
+                        booted = false;
+                    }
+                    break;
+            }
+        }
+
+        public void ReturnAnswer(PromptBuilder builder, SpeechSynthesizer synth, string response)
+        {
+            try
+            {
+                builder.ClearContent();
+                builder.AppendText(response);
+                synth.Speak(builder);                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ SpeechApplication/Spraak2.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpeechApplication
+{
+    public class Spraak2 : Spraak
+    {
+
+        private SpeechSynthesizer Synth = new SpeechSynthesizer();
+        private PromptBuilder builder = new PromptBuilder();
+        private SpeechRecognitionEngine engine = null;
+        private Choices choices = new Choices();
+        private GrammarBuilder gramBuilder = null;
+        private Grammar gram = null;
+
+        public Spraak2()
+        {
+            Func();
+        }
+
+        private void Func()
+        {
+            Synth.SelectVoiceByHints(VoiceGender.Male);
+            choices.Add(new string[]{
+                "Stop Chrome",
+                "Boot Visual Studio",
+                "Start Calculator",
+                "Start Chrome",
+                "Lock",
+                "Youtube Regular",
+                "Boot Notepad++",
+                "Boot Eclipse",
+                "Start Verkenner",
+                "Start Discord",
+                "Screenshot",
+                "Skype",
+                "Visual Studio Code",
+                "Start Taskmanager",
+            });
+            engine = new SpeechRecognitionEngine();
+            gramBuilder = new GrammarBuilder(choices);
+            gramBuilder.Culture = Thread.CurrentThread.CurrentCulture;
+            gram = new Grammar(gramBuilder);
+            engine.LoadGrammar(gram);
+            ReturnAnswer(builder, Synth, "Spraakprogramma 2 started.");
+        }
+        public void Start()
+        {
+            try
+            {
+                Func();
+
+                engine.RequestRecognizerUpdate();
+                engine.SpeechRecognized += engine_SpeechRecognized;
+                engine.SetInputToDefaultAudioDevice();
+                engine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                engine.RecognizeAsyncStop();
+                ReturnAnswer(builder, Synth, "Programma 2 stopped.");
+                engine = null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        [DllImport("user32")]
+        public static extern void LockWorkStation();
+
+        void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString())
+            {
+                case "Start Calculator":
+                    Process.Start("calc");
+                    //ReturnAnswer(builder, Synth, "Calculator Started");
+                    break;
+                case "Start Chrome":
+                    Process.Start("Chrome");
+                    ReturnAnswer(builder, Synth, "Chrome Browser started");
+                    break;
+                case "Lock":
+                    LockWorkStation();
+                    break;
+                case "Youtube Regular":
+                    Process.Start("Chrome", "https://www.youtube.com/watch?v=JRfuAukYTKg&index=1&list=PLPhF6hYLIIMi5hj-ZiCyHm52akRDDpRNC");
+                    ReturnAnswer(builder, Synth, "Regular Playlist started");
+                    break;
+                case "Boot Visual Studio":
+                    Process.Start("devenv");
+                    ReturnAnswer(builder, Synth, "Visual Studio Started.");
+                    break;
+                case "Boot Notepad++":
+                    Process.Start("Notepad++");
+                    ReturnAnswer(builder, Synth, "Notepad++ Started.");
+                    break;
+                case "Boot Eclipse":
+                    Process.Start(@"C:\Users\11400747\eclipse\java-mars\eclipse\eclipse.exe");
+                    ReturnAnswer(builder, Synth, "Eclipse Started.");
+                    break;
+                case "Start Verkenner":
+                    Process.Start("explorer");
+                    ReturnAnswer(builder, Synth, "Explorer Opened.");
+                    break;
+                case "Start Discord":
+                    Process.Start(@"C:\Users\11400747\AppData\Local\Discord\Update.exe");
+                    ReturnAnswer(builder, Synth, "Discord Started.");
+                    break;
+                case "Screenshot":
+                    Process.Start(@"C:\Program Files (x86)\Skillbrains\lightshot\Lightshot.exe");
+                    ReturnAnswer(builder, Synth, "Lightshot started.");
+                    break;
+                case "Skype":
+                    Process.Start("Skype");
+                    ReturnAnswer(builder, Synth, "Skype Booted.");
+                    break;
+                case "Visual Studio Code":
+                    Process.Start("Code");
+                    ReturnAnswer(builder, Synth, "Visual Studio Code Started.");
+                    break;
+                case "Start Taskmanager":
+                    Process.Start("Taskmgr");
+                    ReturnAnswer(builder, Synth, "Task Manager activated.");
+                    break;
+                case "Stop Chrome":
+                    Process[] proc = Process.GetProcessesByName("Chrome");
+                    foreach (Process p in proc)
+                    {
+                        p.Kill();
+                    }
+                    ReturnAnswer(builder, Synth, "Chrome Processes terminated.");
+                    break;
+
+                default:
+                    ReturnAnswer(builder, Synth, "Command Not found.");
+                    break;
+            }
+        }
+    }
+}
